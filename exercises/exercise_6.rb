295c89f9f70f7739f56e0bbf833f4b79aca13326
Leilani <pquaby@gmail.com>
2024-06-28 12:41:18 -0400

    Complete exercise 6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,16 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Quinoa", last_name: "Cat", hourly_rate: 60)
+@store1.employees.create(first_name: "Artemis", last_name: "Cat", hourly_rate: 100)
+@store1.employees.create(first_name: "Finny", last_name: "Dog", hourly_rate: 90)
+
+@store2.employees.create(first_name: "Tufa", last_name: "Dog", hourly_rate: 75)
+@store2.employees.create(first_name: "Sesame", last_name: "Cat", hourly_rate: 50)
+@store2.employees.create(first_name: "Noodle", last_name: "Rabbit", hourly_rate: 40)
+
+@employees = Employee.all
+
+@employees.each do |employee|
+  puts "#{employee.first_name}, #{employee.last_name}, #{employee.hourly_rate}"
+end
