5eb5b181c5962b2e997b123914512e06508874ed
Leilani <pquaby@gmail.com>
2024-06-28 13:11:05 -0400

    Complete exercise 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,16 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+print "Enter store name:"
+@store_name = gets.chomp
+
+@new_store = Store.create(name: @store_name)
+
+if @new_store.save
+  puts "Store created!"
+else
+  puts "Error! Cannot create store"
+  @new_store.errors.full_messages.each do |message|
+    puts message
+  end
+end
