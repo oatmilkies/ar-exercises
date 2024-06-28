65f5967760960dc8e0aba461b6bfe42f506649cc
Leilani <pquaby@gmail.com>
2024-06-28 12:14:52 -0400

    Assign variables store1 and store2 and update store1

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,12 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.find_by(id: 1)
+@store2 = Store.find_by(id: 2)
+
+puts @store1.inspect
+puts @store2.inspect
+
+@store1.update(name: 'Hamilton')
+
+puts @store1.inspect
